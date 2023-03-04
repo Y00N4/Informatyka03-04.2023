1b72cb5a7d0636de83f15bab6d8369c7f6ca16cd
Y00N4 <126967988+Y00N4@users.noreply.github.com>
2023-03-04 15:48:26 +0100

    Add files via upload

--- CoToZaRoznica.cpp
+++ CoToZaRoznica.cpp
@@ -6,17 +6,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n";
-    cout << "Hello World!\n"; 
-    cout << "Hello World!\n";
+    for (int i = 0; i < 10; i++)
+        cout << "Hello World!\n";
 }
 
