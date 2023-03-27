d3b26e30146a32f5a749937a40de5ea58f40153d
Y00N4 <126967988+Y00N4@users.noreply.github.com>
2023-03-27 19:54:54 +0200

    Update CoToZaRoznica.cpp

--- CoToZaRoznica.cpp
+++ CoToZaRoznica.cpp
@@ -96,4 +96,47 @@ int main()
     }
 
 }
+//zadanie 69.
+                          
+#include <iostream>
+#include <string>
+#include <map>
 
+using namespace std;
+
+int main() {
+    string napis = "W roku Pańskim 1345, władca Henryk 12, na rzecz swoich 143209 poddanych uchwalił dekret o 20 procentowej zniżce podatków";
+    map<int, int> liczby;
+
+    int suma = 0;
+    string aktualna_liczba = "";
+
+    for (char znak : napis) {
+        if (znak >= '0' && znak <= '9') {
+            aktualna_liczba += znak;
+        }
+        else if (!aktualna_liczba.empty()) {
+            int liczba = stoi(aktualna_liczba);
+            suma += liczba;
+            liczby[liczba]++;
+            aktualna_liczba = "";
+        }
+    }
+
+    if (!aktualna_liczba.empty()) {
+        int liczba = stoi(aktualna_liczba);
+        suma += liczba;
+        liczby[liczba]++;
+    }
+
+    cout << "Suma liczb: " << suma << endl;
+    cout << "Ilość wystąpień poszczególnych liczb:" << endl;
+    for (auto para : liczby) {
+        cout << para.first << ": " << para.second << endl;
+    }
+
+    return 0;
+}
+
+                          
+                          
