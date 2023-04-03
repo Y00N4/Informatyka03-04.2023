d9b68fd1fcb7bac76f1e7aaa71643ea0d2c2b090
Y00N4 <126967988+Y00N4@users.noreply.github.com>
2023-04-03 20:41:29 +0200

    Update CoToZaRoznica.cpp

--- CoToZaRoznica.cpp
+++ CoToZaRoznica.cpp
@@ -212,6 +212,83 @@ int main() {
 
     return 0;
 }
+                          
+     // 28.03                     farmerzy rolniki itd
+    #include <iostream>
 
+using namespace std;
+
+//MLECZARNIA
+
+struct Dostawca {
+    int numer_rolnika;
+    string nazwa_regionu;
+    string nazwisko;
+    string imie;
+    float ilosc_litrow_mleka;
+    float zawartosc_tluszczu;
+};
+
+int main()
+{
+    Dostawca tab[5] = { {1,"Region Pierwszy","Nazwisko 1","Imie 1",100,2.0},
+                    {2,"Region Drugi","Nazwisko 2","Imie 2",150,3.5},
+                    {3,"Region Trzeci","Nazwisko 3","Imie 3",235,3.0},
+                    {4,"Region Czwarty","Nazwisko 4","Imie 4",346,3.5},
+                    {5,"Region Piaty","Nazwisko 5","Imie 5",100,2.0} };
+
+    //1
+    for (auto element : tab) {
+        cout << "nr. " << element.numer_rolnika << endl;
+        cout << "region: " << element.nazwa_regionu << endl;
+        cout << "Mleczarz: " << element.nazwisko << " " << element.imie << endl;
+        cout << "Litry mleka: " << element.ilosc_litrow_mleka << endl;
+        cout << "Zawartosc tlusczu (%): " << element.zawartosc_tluszczu << endl;
+    }
+    //2 i 3
+    float suma = 0.0;
+    float srednia = 0.0;
+    for (auto element : tab) {
+        suma += element.ilosc_litrow_mleka;
+        srednia += element.zawartosc_tluszczu;
+    }
+    srednia = srednia / 5;
+
+    cout << " " << endl;
+    cout << "Suma litroww mleka: " << suma << endl;
+    cout << "Srednia zawartosc tluszczu: " << srednia << endl;
+    /* Wylicz wyp³aty dla pracowników na podstawie za ka¿dy litr --> 1.15 zl
+    jeœli >150 l mleka to do litra dop³ata 0.5 z³
+    jeœli mniej ni¿ 3.8% to --> 0,4z³ dop³aty */
+
+    //4
+
+
+    float wyplata = 0.0;
+    float sumaWyplat = 0.0;
+
+    cout << " " << endl;
+
+    for (auto element : tab) {
+        wyplata = element.ilosc_litrow_mleka * 1.15;
+
+        if (element.ilosc_litrow_mleka > 150) {
+            wyplata += element.ilosc_litrow_mleka * 0.5;
+        }
+        if (element.zawartosc_tluszczu < 3.8) {
+            wyplata += element.ilosc_litrow_mleka * 0.4;
+        }
+        cout << "Wyplata dla rolnika " << element.numer_rolnika << " wynosi: " << wyplata << endl;
+        sumaWyplat += wyplata;
+        wyplata = 0.0;
+    }
+    cout << " " << endl;
+    cout << "Suma wyplat: " << sumaWyplat << endl;
+
+    return 0;
+}
+                          
+
+                      
                           
                           
